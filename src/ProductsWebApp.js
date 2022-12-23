b850174b9ae3114c08ca5e7ea552526666b4059b
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-12-22 22:58:30 -0500

    create proyect web app products Los Alpes Store

---
+++ src/ProductsWebApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ProductList} from './/components/ProductList';
+
+
+export function App() {
+  return (
+    <>
+      <h1>Los Alpes Store - Product List</h1>
+      <ProductList/>
+    </>
+  );
+}
+
+export default App;
