f8d12352d267bc8d43312c9febf640d358269e85
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-12-27 16:23:48 -0500

    Style vertical orientation Card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -45,46 +45,39 @@ const ProductCard = ({ product, deleteProduct, editProduct }) => {
           onMouseLeave={handleMouseLeave}
           onClick={handleShow}
         >
-          <div className="row g-0 align-items-center">
-            <div className="col-md-3">
-              <img src={product.image} alt={product.name}  className="img-fluid" />
-            </div>
-            <div className="col-md-9">
-              <div className="card-body ">
-                <div className="row g-0">
-                  <div className="col-md-6">
-                    <h2 className="card-title fs-4 ps-2">{product.name}</h2>
-                    <p className="card-text fs-6 ps-2">${product.price}</p>
-                    <div>
-                      {showButton && (
-                        <button
-                          className="btn btn-danger"
-                          onMouseEnter={() => setShowButton(true)}
-                          onMouseLeave={() => setShowButton(false)}
-                          onClick={handleDelete}
-                        >
-                          Delete Item
-                        </button>
-                      )}
+          <div className="d-flex flex-column">
+            {showButton && (
+              <button
+                className="btn btn-danger"
+                onMouseEnter={() => setShowButton(true)}
+                onMouseLeave={() => setShowButton(false)}
+                onClick={handleDelete}
+              >
+                Delete Item
+              </button>
+            )}
+            <img src={product.image} alt={product.name}  className="img-fluid" />
+              <div className="card-body bg-light">
+                <div className="col-md-12">
+                  <h2 className="card-title fs-4 ps-2">{product.name}</h2>
+                  <p className="card-text fs-6 ps-2">${product.price}</p>
+                </div>
+                  {showDescription && (
+                  <div
+                      className="description"
+                      onMouseEnter={() => setShowDescription(true)}
+                      onMouseLeave={() => setShowDescription(false)}
+                    >
+                    <hr />
+                    <div className="card-description ps-2">
+                      <h3 className="card-description-title fs-5">Description</h3>
+                      <p>
+                        {product.description}
+                      </p>
                     </div>
                   </div>
-                  <div className="col-md-6">
-                      {showDescription && (
-                      <div
-                        className="description"
-                        onMouseEnter={() => setShowDescription(true)}
-                        onMouseLeave={() => setShowDescription(false)}
-                      >
-                        <h3 className="card-description fs-4 ps-2">Description</h3>
-                        <p>
-                          {product.description}
-                        </p>
-                      </div>
-                        )}
-                  </div>
-                </div>
+                    )}
               </div>
-            </div>
           </div>
         </div>
       </div>
